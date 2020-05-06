7b11369948634b7677ac862852ba7a84dd6721da
D3viii <devinsdevs@gmail.com>
2020-05-06 18:46:31 -0400

    Profile Pic

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+             <footer className="footer">
+    <div className="two-toned-footer-color"></div>
+    <p className="text-muted text-muted-footer text-center">
+      &copy; Made with Love!
+    </p>
+  </footer>
+        </div>
+    );
+};
+
+export default Footer;
