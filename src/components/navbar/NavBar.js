b0856db5929a24337e2401732de52ec60aadf249
D3viii <devinsdevs@gmail.com>
2021-01-08 11:38:49 -0500

    dfadf

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -12,28 +12,27 @@ const NavBar = () => {
             className="navbar-toggler"
             type="button"
             data-toggle="collapse"
-            data-target="nav-item"
+            data-target="#navbarTogglerDemo01"
             aria-controls="navbarTogglerDemo01"
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon" id="nameNAV"></span>
-
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-              <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link" href="/portfolio">
-                    Portfolio
-                  </a>
-                </li>
-                <li className="nav-item active">
-                  <a className="nav-link" href="/">
-                    About
-                  </a>
-                </li>
-              </ul>
-            </div>
           </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="/portfolio">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </div>
