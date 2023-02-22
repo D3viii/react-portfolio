848cadc0aa2fbf479eee0f6bb0ca6b5979b540ed
Devin Carr <devinsdevs@gmail.com>
2023-02-22 13:45:05 -0500

    Fixed Navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -23,7 +23,7 @@ const NavBar = () => {
             <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
               <li className="nav-item">
                 <a className="nav-link" href="/portfolio">
-                  Jr. Software Engineer
+                  Software Developer
                 </a>
               </li>
              
